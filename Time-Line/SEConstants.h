0e61e6c2c533084956cc0b1119f5483b86267629
yanjian <qq83659105@163.com>
2015-09-18 12:04:57 +0800

    message

---
+++ Time-Line/SEConstants.h
@@ -0,0 +1,19 @@
+//
+//  SEConstants.h
+//  SECoreTextView-iOS
+//
+//  Created by kishikawa katsumi on 2013/04/27.
+//  Copyright (c) 2013 kishikawa katsumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SECompatibility.h"
+
+@interface SEConstants : NSObject
+
++ (UIColor *)selectedTextBackgroundColor;
++ (UIColor *)linkColor;
++ (UIColor *)selectionCaretColor;
++ (UIColor *)caretColor;
+
+@end
